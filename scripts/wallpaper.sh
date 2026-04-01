2c4176580161dcb16b92fbc09b953619761591d0
aivan6 <ivysocriy4@gmail.com>
2026-04-01 18:13:49 +0700

    fix: change update

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -8,6 +8,6 @@ sleep 0.5
 
 while true; do
   WALL=$(find "$DIR" -type f | shuf -n 1)
-  swww img "$WALL" --transition-type fade --transition-duration 1.5
+  awww img "$WALL" --transition-type fade --transition-duration 1.5
   sleep 3600
 done
